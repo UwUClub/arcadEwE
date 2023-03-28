ba8ac5dfb355a290d7d2ef48d302f92582fc2a4c
GlassAlo <maxence.labourel@epitech.eu>
2023-03-28 14:13:21 +0200

    Added more doc

--- src/Core/Core.hpp
+++ src/Core/Core.hpp
@@ -14,19 +14,55 @@
 
 namespace Arcade::Core
 {
+    /**
+     * @brief Core class of the Arcade
+     */
     class Core : public ICore
     {
         public:
-        Core(const std::string &pathDisplay);
+        /**
+         * @brief Construct a new Core object
+         *
+         * @param pathDisplay Path to the display module
+         */
+        explicit Core(const std::string &pathDisplay);
+        /**
+         * @brief Destroy the Core object
+         */
         ~Core() override = default;
 
+        /**
+         * @brief This function is the main loop of the Arcade
+         * It will handle the events and update the display and the game
+         */
         void update() override;
 
         private:
+        /**
+         * @brief This function is used to load the game module
+         *
+         * @param path Path to the game module
+         */
         void loadGameModule(const std::string &path);
+        /**
+         * @brief This function is used to load the game module
+         * It changes the game module to the next one in the list
+         */
         void loadGameModule();
+        /**
+         * @brief This function is used to load the display module
+         *
+         * @param path Path to the display module
+         */
         void loadDisplayModule(const std::string &path);
+        /**
+         * @brief This function is used to load the display module
+         * It changes the display module to the next one in the list
+         */
         void loadDisplayModule();
+        /**
+         * @brief This function is used to handle the events of the Arcade
+         */
         void handleCoreEvents();
 
         std::unique_ptr<GameHandler> _gameModule;
--- src/ECS/EntityManager.hpp
+++ src/ECS/EntityManager.hpp
@@ -9,20 +9,68 @@
 
 namespace Arcade::ECS
 {
+    /**
+     * @brief Base class for all entity managers
+     */
     class EntityManager : public IEntityManager
     {
         public:
+        /**
+         * @brief Construct a new Entity Manager object
+         */
         EntityManager();
+        /**
+         * @brief Destroy the Entity Manager object
+         */
         ~EntityManager() override = default;
 
+        /**
+         * @brief Create an entity
+         *
+         * @param id The id of the entity
+         * @return IEntity& The entity
+         */
         IEntity &createEntity(const std::string &id) override;
+        /**
+         * @brief Get the Entities of the entity manager
+         *
+         * @return const std::vector<std::shared_ptr<IEntity>>& The entities of the entity manager
+         */
         [[nodiscard]] const std::vector<std::shared_ptr<IEntity>> &getEntities() const override;
+        /**
+         * @brief Get the Entities of the entity manager by their component type
+         *
+         * @param comp The type of the component
+         * @return std::unique_ptr<std::vector<std::shared_ptr<IEntity>>> The entities that have the
+         * component
+         */
         [[nodiscard]] std::unique_ptr<std::vector<std::shared_ptr<IEntity>>>
         getEntitiesByComponentType(CompType comp) const override;
+        /**
+         * @brief Get the Components of the entity manager by their component type
+         *
+         * @param comp The type of the component
+         * @return std::unique_ptr<std::vector<std::shared_ptr<IComponent>>> The components of the
+         * entity manager
+         */
         [[nodiscard]] std::unique_ptr<std::vector<std::shared_ptr<IComponent>>>
         getComponentsByComponentType(CompType comp) const override;
+        /**
+         * @brief Remove an entity from the entity manager
+         *
+         * @param id The id of the entity
+         */
         void removeEntity(const std::string &id) override;
+        /**
+         * @brief Remove all entities from the entity manager
+         */
         void removeAllEntities() override;
+        /**
+         * @brief Get an entity of the entity manager by its id
+         *
+         * @param id The id of the entity
+         * @return std::shared_ptr<IEntity> The entity
+         */
         [[nodiscard]] std::shared_ptr<IEntity> getEntitiesById(
             const std::string &id) const override;
 
