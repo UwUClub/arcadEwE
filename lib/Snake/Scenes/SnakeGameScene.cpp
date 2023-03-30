eddb0c90aaa6629e1fe929d1adf91ab9e8d3dd03
macgameur <maceo.jalbert@epitech.eu>
2023-03-30 17:35:38 +0200

    Add entities to Snakegame

--- lib/Snake/Scenes/SnakeGameScene.cpp
+++ lib/Snake/Scenes/SnakeGameScene.cpp
@@ -29,6 +29,14 @@ namespace Snake {
         entity.addComponent(std::shared_ptr<Snake::Direction>(new Snake::Direction("direction", Direction::dir::UP)));
         entity.addComponent(std::shared_ptr<Snake::Speed>(new Snake::Speed("speed", 0.1f)));
         entity.addComponent(std::shared_ptr<Snake::Sprite>(new Snake::Sprite("sprite", SNAKE_PATH, {"#", {255, 255, 255, 255}, {0, 0, 0, 255}}, Arcade::Vector3f(50, 50, 0), {0, 0, 1, 1}, 0)));
+        auto &entity2 = _entityManager->createEntity("snake_body");
+        entity2.addComponent(std::shared_ptr<Snake::Position>(new Snake::Position("position", Arcade::Vector3f(50, 60, 0))));
+        entity2.addComponent(std::shared_ptr<Snake::Direction>(new Snake::Direction("direction", Direction::dir::UP)));
+        entity2.addComponent(std::shared_ptr<Snake::Speed>(new Snake::Speed("speed", 0.1f)));
+        entity2.addComponent(std::shared_ptr<Snake::Sprite>(new Snake::Sprite("sprite", SNAKE_PATH, {"#", {255, 255, 255, 255}, {0, 0, 0, 255}}, Arcade::Vector3f(50, 60, 0), {0, 0, 1, 1}, 0)));
+
+        auto &entity3 = _entityManager->createEntity("fruit_spawn_manager");
+        (void)(entity3);
         return true;
     }
 }
