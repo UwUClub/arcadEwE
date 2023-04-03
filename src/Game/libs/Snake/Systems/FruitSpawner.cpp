d5c892d61577b18e4d858982e88e6d87ff156b2d
macgameur <maceo.jalbert@epitech.eu>
2023-04-03 11:08:09 +0200

    Fix Fruit spawner

--- src/Game/libs/Snake/Systems/FruitSpawner.cpp
+++ src/Game/libs/Snake/Systems/FruitSpawner.cpp
@@ -9,6 +9,9 @@
 #include "Events.hpp"
 #include "ObjectFactory.hpp"
 #include "SnakeGameScene.hpp"
+#include "Transform.hpp"
+#include "BoxCollider.hpp"
+#include "IEntity.hpp"
 #include <cstdlib>
 #include <time.h>
 #include <vector>
@@ -29,40 +32,54 @@ namespace Snake {
     {
         Arcade::Vector3f position = {0, 0, 0};
         ObjectFactory objectFactory;
-        auto fruit = objectFactory.CreateFruit(entityManager, position);
+        auto &fruit = objectFactory.CreateFruit(entityManager, position);
         srand(time(0));
         std::vector<Arcade::Vector3f> positions;
+        auto transforms = fruit.getComponents(Arcade::ECS::CompType::TRANSFORM);
 
-        for (int x = 0; x < MAX_CASE_X; x++) {
-            for (int y = 0; y < MAX_CASE_Y; y++) {
-                fruit.getComponents(Arcade::ECS::CompType::TRANSFORM).SetPosition = {x * CASE_SIZE, y * CASE_SIZE, 0};
-                if (isFruitColliding(entityManager) == false)
-                    positions.push_back(fruit.getComponent<Transform>(Arcade::ECS::CompType::TRANSFORM).GetPosition);
+        for (float x = 0; x < MAX_CASE_X; x++) {
+            for (float y = 0; y < MAX_CASE_Y; y++) {
+
+                for (auto &transform : transforms) {
+                    auto &transformRef = dynamic_cast<Snake::Transform &>(*transform);
+                    transformRef.setPosition({x * CASE_SIZE, y * CASE_SIZE, 0});
+                    if (isFruitColliding(entityManager) == false)
+                        positions.push_back({x * CASE_SIZE, y * CASE_SIZE, 0});
+                }
             }
         }
         if (positions.size() > 0) {
             int random = rand() % positions.size();
-            fruit.getComponents(Arcade::ECS::CompType::TRANSFORM).SetPosition = positions[random];
+            for (auto &transform : transforms) {
+                auto &transformRef = dynamic_cast<Snake::Transform &>(*transform);
+                transformRef.setPosition(positions[random]);
+            }
             return true;
         }
         return false;
     }
 
     bool FruitSpawner::isFruitColliding(Arcade::ECS::IEntityManager &entityManager) {
-        auto &entities = entityManager.getEntitiesByComponentType(Arcade::ECS::CompType::COLLIDER);
-        auto &fruit = entityManager.getEntitiesById("fruit");
+        auto entities = *entityManager.getEntitiesByComponentType(Arcade::ECS::CompType::COLLIDER);
+        auto fruit = entityManager.getEntitiesById("fruit");
 
-        for (auto &entity : entities) {
-            auto &collider = entity->getComponent<BoxCollider>(Arcade::ECS::CompType::COLLIDER);
+        for (auto entity : entities) {
+            auto colliders = entity->getComponents(Arcade::ECS::CompType::COLLIDER);
+            if (colliders.size() == 0 || !colliders[0])
+                continue;
+            auto &collider = dynamic_cast<BoxCollider &>(*colliders[0]);
             if (!collider.isEnabled)
                 continue;
             for (auto &other : entities) {
                 if (entity == other)
                     continue;
-                auto &otherCollider = other->getComponent<BoxCollider>(Arcade::ECS::CompType::COLLIDER);
+                auto otherColliders = other->getComponents(Arcade::ECS::CompType::COLLIDER);
+                if (otherColliders.size() == 0 || !otherColliders[0])
+                    continue;
+                auto &otherCollider = dynamic_cast<BoxCollider &>(*otherColliders[0]);
                 if (!otherCollider.isEnabled)
                     continue;
-                if (collider.isColliding(otherCollider)) {
+                if (collider.IsColliding(otherCollider)) {
                     return true;
                 }
             }
