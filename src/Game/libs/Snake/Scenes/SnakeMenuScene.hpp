daae0bbcd7dcc491f57797d6a9cc3c05b5d0d5be
macgameur <maceo.jalbert@epitech.eu>
2023-04-04 20:41:34 +0200

    Center Snake menu button

--- src/Game/libs/Snake/Scenes/SnakeMenuScene.hpp
+++ src/Game/libs/Snake/Scenes/SnakeMenuScene.hpp
@@ -8,7 +8,7 @@
 #ifndef SNAKEMENUSCENE_HPP_
     #define SNAKEMENUSCENE_HPP_
     #include "SnakeScene.hpp"
-    #define BASE_X        20
+    #define BASE_X        40
     #define OFFSET_Y_TEXT 50
     #define OFFSET_X_TEXT 180
     #define FONT_PATH "assets/fonts/arial.ttf"
